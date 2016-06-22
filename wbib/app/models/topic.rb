55da05d83254717e1ff9536a07488a80fc6e354f
mwoolsey <mwoolsey625@gmail.com>
2016-06-21 18:33:26 -0700

    completed assignment

--- wbib/app/models/topic.rb
+++ wbib/app/models/topic.rb
@@ -3,4 +3,7 @@ class Topic < ActiveRecord::Base
   # gives extra functions to interact with references (@topic.references is one)
   has_many :references, dependent: :destroy
 
+  validates :title, presence: true
+  validates :description, presence: true
+
 end
